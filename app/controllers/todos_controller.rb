842af384225f8a38f2130ecca7003d93e4f6a35b
Sofiaimg <sofia.img@gmail.com>
2017-10-30 16:09:40 -0300

    Prueba Todo app rev

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -31,13 +31,6 @@ class TodosController < ApplicationController
     redirect_to todos_path
   end
 
-  # def complete
-  #  todo = Todo.find(params[:id])
-  #  @todo.completed = true
-  #  @todo.save
-  #  redirect_to todos_path
-  #end
-
   def list
     @todos = Todo.all
   end
